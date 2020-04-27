97732d9a08d7439d963fad15a8d30f7a91d97193
Viktor Evdokimov <viktor.evdokimov@instacart.com>
2020-04-27 00:37:16 -0700

    add missing model. fix welcome controller. add root route

--- carrot-u-admin/config/routes.rb
+++ carrot-u-admin/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/", to: "welcome#index"
-
+  root 'welcome#index'
 
   # okta saml authenticates by posting to the callback url
   get 'sessions/create'
