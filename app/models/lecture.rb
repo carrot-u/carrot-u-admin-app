ee22a86d1ef7f7766c6a1ec14e0dcb71c4b09a0e
Bradley Dixion <bradley.dixon@instacart.com>
2020-06-15 17:41:28 -0700

    Updates to homework show and back links

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,8 +1,13 @@
 class Lecture < ApplicationRecord
   belongs_to :course_session
+  before_destroy :destroy_homeworks
   has_one :homework
 
   def has_homework?
     !self.homework.nil?
+  end
+
+  def destroy_homeworks
+    self.homework.destroy
   end
 end
