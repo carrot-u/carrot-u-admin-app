1701202968e0884d25e36c8e196380874b6863dc
Bradley Dixion <bradley.dixon@instacart.com>
2020-06-16 18:50:12 -0700

    Updated edits with gem in appropriate space

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,8 +1,16 @@
 class Lecture < ApplicationRecord
   belongs_to :course_session
+  before_destroy :destroy_homeworks
   has_one :homework
 
   def has_homework?
     !self.homework.nil?
   end
+
+  def destroy_homeworks
+    if has_homework?
+      self.homework.destroy
+    end
+  end
+  
 end
