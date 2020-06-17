93df5383a5a68da12c8cb9240eff7632605eeb5c
Robert Border <robert.border@instacart.com>
2020-06-16 21:58:11 -0400

    Remove email used to test myster person placeholder in case of no match on md5 hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   # creates MD5 hash of lowercase email and formats image url for gravatar avatar
   def gravatar_url
-    @md5 = Digest::MD5.hexdigest "bradleydixon@gmail.com"
+    @md5 = Digest::MD5.hexdigest self.email.downcase
     "https://www.gravatar.com/avatar/" + @md5 + "?d=mp"
   end
 end
