386c94236a8cfea344c63525a27eef768be0006c
Robert Border <robert.border@instacart.com>
2020-06-16 21:56:41 -0400

    Create user method to generate gravatar image url, and place avart in sticky footer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
+require "digest"
+
 class User < ApplicationRecord
 
+  # creates MD5 hash of lowercase email and formats image url for gravatar avatar
+  def gravatar_url
+    @md5 = Digest::MD5.hexdigest "bradleydixon@gmail.com"
+    "https://www.gravatar.com/avatar/" + @md5 + "?d=mp"
+  end
 end
