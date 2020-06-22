94873adde1adbd9254277866cd653dd2dfeaf2d8
Robert Border <robert.border@instacart.com>
2020-06-22 19:33:16 -0400

    Add Admin to Users model, and add basic views to validate user admin status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_172921) do
+ActiveRecord::Schema.define(version: 2020_06_22_221841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 2020_06_07_172921) do
     t.string "last_sign_in_ip"
     t.string "id_token"
     t.string "token"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
