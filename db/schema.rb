a46eded1796315c8ec72849ecd9fd8248ad48c42
Annabel Chavez <annabel.chavez@instacart.com>
2020-06-23 19:24:50 -0700

    updating yarn again because i messed up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_233834) do
+ActiveRecord::Schema.define(version: 2020_06_23_182731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,15 +41,6 @@ ActiveRecord::Schema.define(version: 2020_05_07_233834) do
     t.string "repository_link"
     t.bigint "lectures_id"
     t.index ["lectures_id"], name: "index_course_sessions_on_lectures_id"
-  end
-
-  create_table "course_sessions", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
     create_table "homeworks", force: :cascade do |t|
