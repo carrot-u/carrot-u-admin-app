1a16f07e0edf89f555d45dfcc955b90b0d2c48a4
Muffy Barkocy <muffy@instacart.com>
2020-07-02 14:52:18 -0700

    Quick hack to fix db:migrate

--- db/migrate/20200612000802_create_users_managers.rb
+++ db/migrate/20200612000802_create_users_managers.rb
@@ -2,7 +2,7 @@ class CreateUsersManagers < ActiveRecord::Migration[6.0]
   def change
     create_table :users_managers do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :manager, null: false, foreign_key: true
+      t.string :manager_email
       t.boolean :current
 
       t.timestamps
